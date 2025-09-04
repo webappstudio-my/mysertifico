a5ea637f674def8ec2cf2d30a1016ecdd57985b5
fikrirozi147 <fikridh00@gmail.com>
2025-09-04 16:27:52 +0800

    update my profile to use toast component

--- src/components/common/Toast.jsx
+++ src/components/common/Toast.jsx
@@ -23,7 +23,7 @@ const Toast = ({ message, type, onClose }) => {
 
     const iconClasses = {
         success: 'ri-check-line',
-        error: 'ri-error-warning-line',
+        error: 'ri-alarm-warning-fill',
     };
 
     return (
