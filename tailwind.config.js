80554dd0196cd8f77e78370c74df0158b955c74b
AmirulNashreeq <amirulnashreeq1998@gmail.com>
2025-09-09 12:13:38 +0800

    added mywall colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -19,12 +19,34 @@ export default { // Ensure 'export default' for Vite
         'primary-light': '#14b8a6', // Teal-500
         'primary-50': '#f0fdfa',
         'accent': '#F97316', // Orange-500 like in html examples
+        
 
         // Back Office Colors
         'bo-bg-light': '#f8fafc',
         'bo-bg-dark': '#020617',
         'bo-surface-light': '#ffffff',
         'bo-surface-dark': '#1e293b',
+
+        // MyWall Colors
+        // Palet Aqua Lautan & Oren Senja
+        'primary-mywall': {
+            'DEFAULT': '#14B8A6',       // Aqua Lautan (Teal-500)
+            '50': '#f0fdfa',
+            '100': '#ccfbf1',
+            '200': '#99f6e4',
+            '300': '#5eead4',
+            '400': '#2dd4bf',
+            '500': '#14b8a6',
+            '600': '#0d9488',
+            '700': '#0f766e',
+            '800': '#115e59',
+            '900': '#134E4A',    // Aqua Gelap (Teal-900)
+        },
+        'accent-mywall': {
+            'DEFAULT': '#F97316',        // Oren Senja (Orange-500)
+            'hover': '#EA580C',  // Oren Hangat untuk hover (Orange-600)
+        },
+        'dark-bg': '#134E4A',       // Aqua Sangat Gelap untuk footer (Teal-900)
       }
     },
   },
