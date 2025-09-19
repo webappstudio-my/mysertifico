1c7ac96145bed6946c36b52e366ea6a8d9ecaedb
safeaqah <safeaqah08@gmail.com>
2025-09-19 18:13:34 +0800

    CU-86b6ndwev - Parent My Profile

--- src/App.jsx
+++ src/App.jsx
@@ -64,6 +64,7 @@ import MWForgotPassword from './pages/mywall/auth/MWForgotPassword';
 import MyWallSignIn from './pages/mywall/auth/MyWallSignIn';
 import MyFamilyPage from './pages/mywall/parent/MyFamilyPage';
 import ParentEditProfile from './pages/mywall/parent/ParentEditProfile';
+import ParentMyProfile from './pages/mywall/parent/ParentMyProfile';
 import StudentEditProfile from './pages/mywall/student/StudentEditProfile';
 
 
@@ -138,6 +139,7 @@ function App() {
         <Route path='/mywall/auth/sign-in' element={<MyWallSignIn />} />
         <Route path='/mywall/parent-myfamily' element={<MyFamilyPage />} />
         <Route path='/mywall/parent-edit-profile' element={<ParentEditProfile />} />
+<Route path='/mywall/parent-myprofile' element={<ParentMyProfile />} />
         <Route path='/mywall/student-edit-profile' element={<StudentEditProfile />} />
 
         {/* Back Office */}
